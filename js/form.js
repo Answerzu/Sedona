8cb0989bf222b1f74e34c6496307bb2fe50a1347
Answerzu <bbeijing92@gmail.com>
2020-07-28 16:05:45 +0300

    js

--- js/form.js
+++ js/form.js
@@ -1,9 +1,21 @@
 var button = document.querySelector(".search-form");
-var form = document.querySelector(".form")
+var form = document.querySelector(".form");
+var date1 = form.querySelector("[name=date1]");
+var date2 = form.querySelector("[name=date2]");
+var adults = form.querySelector("[name=num1]");
+var children = form.querySelector("[name=num2]");
 
 button.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	form.classList.toggle("form-show");
+	date1.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+	if (!date1.value || !date2.value || adults.value == 0 && children.value == 0) {
+		evt.preventDefault();
+		console.log("Нужно ввести пароль");
+	}
 });
 
 
