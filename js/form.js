88329816736769cc8271d6bde6f1814ddc19360c
Answerzu <bbeijing92@gmail.com>
2020-09-15 17:17:27 +0300

    CamelLanguage
    fixed

--- js/form.js
+++ js/form.js
@@ -1,61 +1,61 @@
-var button = document.querySelector(".search_button_show");
+var button = document.querySelector(".searchButtonShow");
 var form = document.querySelector(".form");
-var arrival = form.querySelector("[name=arrival_date]");
-var departure = form.querySelector("[name=departure_date]");
+var arrival = form.querySelector("[name=arrivalDate]");
+var departure = form.querySelector("[name=departureDate]");
 var adults = form.querySelector("[name=adults]");
 var children = form.querySelector("[name=children]");
-var adults_plus = document.querySelector(".adults_plus");
-var adults_minus = document.querySelector(".adults_minus");
-var children_plus = document.querySelector(".children_plus");
-var children_minus = document.querySelector(".children_minus");
+var adultsPlus = document.querySelector(".adultsPlus");
+var adultsMinus = document.querySelector(".adultsMinus");
+var childrenPlus = document.querySelector(".childrenPlus");
+var childrenMinus = document.querySelector(".childrenMinus");
 
 
 button.addEventListener("click", function (evt) { /* добавляем возможность показать/скрыть форму */
 	evt.preventDefault();
-	form.classList.toggle("form_show");
+	form.classList.toggle("formShow");
 	arrival.focus();
-	form.classList.remove("form_error")
+	form.classList.remove("formError")
 });
 
 form.addEventListener("submit", function (evt) { /* запрящаем отправлять форму, если есть пустые поля*/
 	if (!arrival.value || !departure.value || adults.value == 0 && children.value == 0) {
 		evt.preventDefault();
-		form.classList.remove("form_error");
+		form.classList.remove("formError");
 		form.offsetWidth = form.offsetWidth;
-		form.classList.add("form_error");
+		form.classList.add("formError");
 	}
 });
 
 window.addEventListener("keydown", function (evt) { /*добавляем закрытие форы через Esc*/
 	if (evt.keyCode === 27) {
 		evt.preventDefault();
-		if (form.classList.contains("form_show")) {
+		if (form.classList.contains("formShow")) {
 			evt.preventDefault();
-			form.classList.remove("form_show")
-			form.classList.remove("form_error")
+			form.classList.remove("formShow")
+			form.classList.remove("formError")
 		}
 	}
 });
 
 
-adults_plus.addEventListener("click", function (evt) {
+adultsPlus.addEventListener("click", function (evt) { /*оживляем кнопки плюс/минус*/
 	evt.preventDefault();
 	adults.value = ++adults.value;
 });
 
-adults_minus.addEventListener("click", function (evt) {
+adultsMinus.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	if ( adults.value > 0 ) {
 	adults.value = --adults.value;
 	}
 });
 
-children_plus.addEventListener("click", function (evt) {
+childrenPlus.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	children.value = ++children.value;
 });
 
-children_minus.addEventListener("click", function (evt) {
+childrenMinus.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	if ( children.value > 0 ) {
 	children.value = --children.value;
